00f66f4d2e2b086aacfe820b856e85494af6fae2
Najman <najman.husaini@utah.edu>
2022-10-27 18:48:06 -0600

    implemented test

--- src/telemetry/test/test_message_handler.py
+++ src/telemetry/test/test_message_handler.py
@@ -1,7 +1,11 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath('../telemetry'))
 from message_handler import parse_message, SoftPing
 
 
 def test_soft_ping():
     msg = parse_message(bytes([0, 2, 4]))
     assert isinstance(msg, SoftPing)
-    assert msg.body == [2, 4]
+    assert msg.body == bytes([2, 4])
