fe131855e585cd554e8ca04d832875cf386cc171
Manglemix <shabouza030@gmail.com>
2023-03-25 15:10:31 -0600

    hotfix launch files

--- src/launch/lunabase.py
+++ src/launch/lunabase.py
@@ -4,15 +4,15 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='local_gamepad_input',
-            namespace='local_gamepad_input',
-            executable='gamepad_node',
-            name='local_gamepad_input'
+            package='local_gamepad',
+            # namespace='local_gamepad',
+            executable='local_gamepad',
+            name='local_gamepad'
         ),
         Node(
             package='telemetry',
-            namespace='telemetry_server',
+            # namespace='telemetry_server',
             executable='server',
-            name='telementry_server'
+            name='telemetry_server'
         )
     ])
--- src/launch/lunabot.py
+++ src/launch/lunabot.py
@@ -5,19 +5,16 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='drive',
-            namespace='drive',
             executable='drive',
             name='drive'
         ),
         Node(
             package='telemetry',
-            namespace='telemetry_client',
             executable='client',
             name='telementry_client'
         ),
         Node(
             package='mining_arm',
-            namespace='mining_arm',
             executable='mining_arm',
             name='mining_arm'
         )
